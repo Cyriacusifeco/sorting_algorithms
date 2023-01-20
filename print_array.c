4182d0f5046e1ff1bc9aeb212c29d15a324de555
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-01-20 14:24:20 +0000

    sorting algorithms

---
+++ print_array.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
+
+/*
+ * swap - swaps to integers
+ * @: First integer
+ * @: Second integer
+ * Return: Nothing
+ */
+void swap(int *a, int *b)
+{
+    int tmp;
+
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
